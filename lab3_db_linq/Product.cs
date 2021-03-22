9e51616aae20908963b743904e3ea25e7d6ecf87
Mpkhort <giselle98@mail.ru>
2021-03-22 20:22:23 +0300

    Creation of DB "Store" + 3 linq request

---
+++ lab3_db_linq/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab3_db_linq
+{
+    class Product
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int cost { get; set; }
+        public string vendor { get; set; }
+        public List<Storehouse> Storehouses { get; set; }
+
+    }
+}
